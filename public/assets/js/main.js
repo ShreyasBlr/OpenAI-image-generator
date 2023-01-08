00f44469ac11474e9854c83961ece9d790e343c4
Shreyas <s.shreyas.blr@gmail.com>
2023-01-08 23:04:55 +0530

    UI changes, PWA

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,14 @@
+// Service worker registration
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
+
+// Call backend API for image generation
 function onSubmit(event) {
   event.preventDefault();
   const prompt = document.querySelector("#prompt").value;
@@ -21,19 +32,15 @@ async function generateImageRequest(prompt, size) {
     }
     const { imageUrl } = await response.json();
     document.querySelector(".generated-img").src = imageUrl;
-    hideSpinner();
   } catch (error) {
     hideSpinner();
     console.log(error);
   }
 }
 
+// Show spinner while waiting
 function showSpinner() {
-  document.querySelector(".spinner-container").classList.add("show");
-}
-
-function hideSpinner() {
-  document.querySelector(".spinner-container").classList.remove("show");
+  document.querySelector(".generated-img").src = "assets/img/painting.gif";
 }
 
 // function reset() {
@@ -42,6 +49,7 @@ function hideSpinner() {
 //     "./assets/img/placeholder-img.png";
 // }
 
+// Listen for form submit
 document
   .querySelector(".prompt-size-form")
   .addEventListener("submit", onSubmit);
---
+++ public/serviceWorker.js
@@ -0,0 +1,27 @@
+const imageGenerator = "image-generator-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/assets/css/style.css",
+  "/assets/js/main.js",
+  "/assets/img/OpenAI-logo.png",
+  "/assets/img/overlay_2.jpg",
+  "/assets/img/painting.gif",
+  "/assets/img/placeholder-img.png",
+];
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(imageGenerator).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
